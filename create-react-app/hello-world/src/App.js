43c52451b6bf63001244c5895d7031717895967f
cjewell47 <charles.jewell47@gmail.com>
2017-08-07 17:53:08 +0100

    part 2 complete

--- create-react-app/hello-world/src/App.js
+++ create-react-app/hello-world/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import HelloWorld from './HelloWorld';
 
 
 const App = () => {
-  return (<div className="App">Hello World!</div>);
+  return (
+    <div className="App">
+      <HelloWorld name="Tony" />
+      <HelloWorld name="Paulie" />
+      <HelloWorld name="Silvio" />
+    </div>);
 };
 
 export default App;
