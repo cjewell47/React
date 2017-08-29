6a9b74432e9733dd77ced3e2c49c8c874264409d
cjewell47 <charles.jewell47@gmail.com>
2017-08-29 17:59:30 +0100

    lecture 12

--- ModernReactRedux/newModernReactRedux/src/index.js
+++ ModernReactRedux/newModernReactRedux/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Working</div>;
+  return <div>Working!!</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
