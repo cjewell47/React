756f30338abda8745ce0cf78fa7c9be55000cc07
cjewell47 <charles.jewell47@gmail.com>
2017-08-29 20:08:58 +0100

    lecture 18 complete state

--- ModernReactRedux/newModernReactRedux/src/index.js
+++ ModernReactRedux/newModernReactRedux/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDSu1Lda7GFfCJkvtYG_MHXZw1Dx0KknMQ';
+
 const App = () => {
-  return <div>Working!!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
