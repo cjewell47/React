f5a6b82b13114006a254f33c450419d553bb4527
cjewell47 <charles.jewell47@gmail.com>
2017-08-30 18:02:54 +0100

    lesson 41 complete

---
+++ ModernReactRedux/newModernReactRedux2/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'One Hundred Years of Solitude' },
+    { title: 'Anna Karenina' },
+    { title: 'The Great Gatsby' },
+    { title: 'Wuthering Heights' }
+  ]
+}
