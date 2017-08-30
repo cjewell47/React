51824f463915cf6e24b31bb4db82c3926e6d07e3
cjewell47 <charles.jewell47@gmail.com>
2017-08-30 15:00:50 +0100

    fixed linter issue with dependcies

--- ModernReactRedux/newModernReactRedux/src/index.js
+++ ModernReactRedux/newModernReactRedux/src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDSu1Lda7GFfCJkvtYG_MHXZw1Dx0KknMQ';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'Taylor Swift'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
