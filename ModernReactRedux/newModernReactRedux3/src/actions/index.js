ad35897861b26c32f67c4055ce38552a0b73e3d8
cjewell47 <charles.jewell47@gmail.com>
2017-08-31 16:20:46 +0100

    lecture 58 complete

---
+++ ModernReactRedux/newModernReactRedux3/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'ea9fb47dac28b184fb9f93cdb2456b29';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
