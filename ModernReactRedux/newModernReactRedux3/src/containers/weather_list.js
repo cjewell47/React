06a976015373ee5539bbbe99b88d2c92dc520194
cjewell47 <charles.jewell47@gmail.com>
2017-09-04 17:50:37 +0100

    lecture 67 complete

--- ModernReactRedux/newModernReactRedux3/src/containers/weather_list.js
+++ ModernReactRedux/newModernReactRedux3/src/containers/weather_list.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273.15);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
 
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td><Chart data={temps} color="orange" units="C"/></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="black" units="%" /></td>
       </tr>
     );
   }
