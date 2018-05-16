a7689ac1b31f44aaaf0947f5cc9209865f607695
cjewell47 <charles.jewell47@gmail.com>
2018-05-16 22:54:54 +0100

    init

---
+++ simplefilter/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const people = [
+  {
+    id: 1,
+    first: "Andre",
+    last: "3000",
+    age: 43
+  },
+  {
+    id: 2,
+    first: "Ghostface",
+    last: "Killer",
+    age: 45
+  },
+  {
+    id: 3,
+    first: "Childish",
+    last: "Gambino",
+    age: 30
+  },
+  {
+    id: 4,
+    first: "Frank",
+    last: "Ocean",
+    age: 32
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {
+          people.map(person => {
+            <div key={person.id}>
+              <h1>{person.first}</h1>
+              <h1>{person.first}</h1>
+              <h1>{person.age}</h1>
+            </div>
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
