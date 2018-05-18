5670fc923be380fdb356d35ab9b860ab1be6fd5f
cjewell47 <charles.jewell47@gmail.com>
2018-05-18 23:54:33 +0100

    getting data from api

--- json-request/src/App.js
+++ json-request/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +10,21 @@ class App extends Component {
     this.state = {
       people: [],
     }
+
+    this.getPeople = this.getPeople.bind(this)
+  }
+  getPeople() {
+    return axios.get('https://swapi.co/api/people/')
+    .then((response) => {
+      console.log(response.data.results)
+      this.setState( { people: response.data.results } )
+    })
+  }
+  componentDidMount() {
+    this.getPeople()
   }
   render() {
+    const {people} = this.state;
     return (
       <div className="App">
       </div>
