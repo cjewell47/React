71f2b7438f754c78de687b5ec7245334b4a0091b
cjewell47 <charles.jewell47@gmail.com>
2018-05-19 15:39:47 +0100

    added filter

--- json-request/src/List.jsx
+++ json-request/src/List.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import CharInfo from './CharInfo';
 
+function searchingFor(term) {
+  return function(x) {
+    let length = term.length;
+    console.log('x', x)
+    console.log('term', term)
+    console.log('length', length)
+    return x.name.toLowerCase().startsWith(term.toLowerCase()) || !term;
+  }
+}
+
 class List extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    }
+
+    this.searchHandler = this.searchHandler.bind(this);
+  }
+  searchHandler(e){
+    this.setState({term: e.target.value});
+  }
   render() {
     const people = this.props.people;
     return (
       <div className="">
+        <form>
+          <input type="text"
+              onChange={this.searchHandler}
+              value={this.state.term}
+          />
+        </form>
         {
-          people.map((p) => {
+          people.filter(searchingFor(this.state.term)).map((p) => {
             console.log(p)
             return (
               <div key={p.url}>
